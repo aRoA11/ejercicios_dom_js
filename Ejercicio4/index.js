d7bdfb6467424c1e0be61e597c7359a787717d44
aRoA11 <trigoramos.aroa@gmail.com>
2025-02-07 15:09:06 +0100

    ejercicio 4

--- Ejercicio4/index.js
+++ Ejercicio4/index.js
@@ -1,7 +1,12 @@
-// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el
-
-// evento click que ejecute un console log con la información del evento del click
-
+// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el evento click que ejecute un console log con la información del evento del click
+let btnToClick = document.querySelector('#btnToClick')
+btnToClick.addEventListener('click', function (event) {
+  console.log(event)
+})
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+const focusInput = document.querySelector('.focus')
+focusInput.addEventListener('focus', () => {
+  console.log(focusInput.value)
+})
 
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
